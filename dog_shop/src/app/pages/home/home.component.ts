86e41493b98d1a9a1d9e05e57bbe1c6534439ba6
Thais <thaisfrsoares@gmail.com>
2025-06-19 10:53:28 +0100

    card-component

--- dog_shop/src/app/pages/home/home.component.ts
+++ dog_shop/src/app/pages/home/home.component.ts
@@ -7,20 +7,6 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   imports: [ProductCardComponent, CommonModule],
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
